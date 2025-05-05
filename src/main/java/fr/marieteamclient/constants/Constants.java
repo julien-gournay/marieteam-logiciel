2344f736a71e41eb749e9d75575339ef064f4a3d
Julien Gournay <julien.grny@gmail.com>
2025-05-05 12:49:25 +0200

    Ajout commentaire

--- src/main/java/fr/marieteamclient/constants/Constants.java
+++ src/main/java/fr/marieteamclient/constants/Constants.java
@@ -6,6 +6,7 @@ import java.util.Properties;
 /**
  * Classe contenant les constantes de l'application.
  * Gère les paramètres de configuration de l'application et de la base de données.
+ * Cette classe est final et ne peut pas être instanciée.
  */
 public final class Constants {
 
@@ -16,6 +17,11 @@ public final class Constants {
     /** Mot de passe pour la connexion à la base de données */
     public static final String DATABASE_PASSWORD;
 
+    /**
+     * Bloc d'initialisation statique pour charger les propriétés de la base de données.
+     * Charge les propriétés depuis le fichier .properties et initialise les constantes de connexion.
+     * @throws RuntimeException si le fichier de propriétés ne peut pas être chargé
+     */
     static {
         Properties props = new Properties();
         try(FileInputStream input = new FileInputStream(".properties")) {
@@ -32,11 +38,11 @@ public final class Constants {
     public static final String APP_NAME = "MarieTeam Client";
     /** Version de l'application */
     public static final String APP_VERSION = "1.0";
-    /** Largeur par défaut de la fenêtre de l'application */
+    /** Largeur par défaut de la fenêtre de l'application en pixels */
     public static final double APP_WIDTH = 1200;
-    /** Hauteur par défaut de la fenêtre de l'application */
+    /** Hauteur par défaut de la fenêtre de l'application en pixels */
     public static final double APP_HEIGHT = 700;
 
-    /** Intervalle de vérification de la connexion à la base de données (3 minutes) */
+    /** Intervalle de vérification de la connexion à la base de données en millisecondes (3 minutes) */
     public static final int DATABASE_CHECK_INTERVAL = 3 * 60 * 1000;
 }
